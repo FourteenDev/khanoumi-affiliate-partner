6dafaac7d024b2318e604b6f5ff354b92d7d85b7
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-07-04 02:23:08 +0330

    Add tabs to settings

--- views/admin/settings/wrapper.php
+++ views/admin/settings/wrapper.php
@@ -1,13 +1,34 @@
-<div class="wrap btbp-wrap">
-	<h1><?php esc_html_e('Settings', KAPP_TEXT_DOMAIN); ?></h1>
+<div class="wrap kapp-wrap">
+	<div class="kapp-wrap__main">
+		<h1><?php esc_html_e('Settings', KAPP_TEXT_DOMAIN); ?></h1>
 
-	<form method="post" action="options.php">
-		<?php
-		settings_fields(KAPP_SETTINGS_SLUG . '_settings_group');
+		<div class="kapp-tab-group">
+			<ul class="kapp-tab">
+				<?php
+				foreach ($tabs as $tabSlug => $tabLabel)
+				{
+					$tabSlug = sanitize_key($tabSlug);
+					$tabUrl  = add_query_arg([
+						'settings-updated' => false,
+						'tab'              => $tabSlug,
+					]);
 
-		do_settings_sections(KAPP_SETTINGS_SLUG . '_settings');
+					echo '<li class="tab-' . $tabSlug . '"><a href="' . esc_url($tabUrl) . '" title="' . esc_attr($tabLabel) . '" class="' . ($activeTab == $tabSlug ? 'active' : '') . '">';
+						echo $tabLabel;
+					echo '</a></li>';
+				} ?>
+			</ul>
+			<div class="kapp-tab-content <?php echo sanitize_key($activeTab); ?>-settings-tab">
+				<form method="post" action="options.php">
+					<table class="form-table">
+						<?php
+						settings_fields(KAPP_SETTINGS_SLUG . '_settings_group');
+						do_settings_fields(KAPP_SETTINGS_SLUG . '_settings', KAPP_SETTINGS_SLUG . "_settings_$activeTab"); ?>
+					</table>
 
-		submit_button();
-		?>
-	</form>
+					<?php submit_button(); ?>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
