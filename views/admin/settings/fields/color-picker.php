1008dce8bdc0c7faade2d406511f9191be03facc
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-08-10 00:04:33 +0330

    Add number settings field

--- views/admin/settings/fields/color-picker.php
+++ views/admin/settings/fields/color-picker.php
@@ -1,4 +1,4 @@
-<input type="text" id="<?php echo $id; ?>" class="kapp-color-picker" name="<?php echo $name; ?>" value="<?php echo esc_attr($value); ?>" data-default-color="<?php echo !empty($default) ? esc_attr($default) : ''; ?>" />
+<input type="text" id="<?php echo esc_attr($id); ?>" class="kapp-color-picker" name="<?php echo esc_attr($name); ?>" value="<?php echo esc_attr($value); ?>" data-default-color="<?php echo !empty($default) ? esc_attr($default) : ''; ?>" />
 <?php if (!empty($description)) : ?>
 	<p><?php echo $description; ?></p>
 <?php endif; ?>
