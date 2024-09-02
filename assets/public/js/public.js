beaa78ec710372b087cf99e2a7edfa644759dcc8
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-03 02:30:34 +0330

    Wrap JS codes in document ready event

--- assets/public/js/public.js
+++ assets/public/js/public.js
@@ -1,42 +1,45 @@
 (function($)
 {
-	$('.khanoumi-carousel').each(function() { setProductsSlickCarousel($(this)) });
-
-	function setProductsSlickCarousel($parentElement)
+	$(document).ready(function($)
 	{
-		var $slickArgs = {
-			rtl: true,
-			autoplay: true,
-			infinite: true,
-			dots: false,
-			autoplaySpeed: $parentElement.data('speed') !== undefined ? $parentElement.data('speed') : 3000,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			prevArrow: $parentElement.find('.khanoumi-carousel__control-prev'),
-			nextArrow: $parentElement.find('.khanoumi-carousel__control-next'),
-			cssEase: 'linear',
-			lazyLoad: 'anticipated',
-			responsive: [
-				{
-					breakpoint: 780,
-					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 3,
-						infinite: true,
-						dots: false,
+		$('.khanoumi-carousel').each(function() { setProductsSlickCarousel($(this)) });
+
+		function setProductsSlickCarousel($parentElement)
+		{
+			var $slickArgs = {
+				rtl: true,
+				autoplay: true,
+				infinite: true,
+				dots: false,
+				autoplaySpeed: $parentElement.data('speed') !== undefined ? $parentElement.data('speed') : 3000,
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				prevArrow: $parentElement.find('.khanoumi-carousel__control-prev'),
+				nextArrow: $parentElement.find('.khanoumi-carousel__control-next'),
+				cssEase: 'linear',
+				lazyLoad: 'anticipated',
+				responsive: [
+					{
+						breakpoint: 780,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: false,
+						}
+					},
+					{
+						breakpoint: 380,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+							infinite: true,
+							dots: false,
+						}
 					}
-				},
-				{
-					breakpoint: 380,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1,
-						infinite: true,
-						dots: false,
-					}
-				}
-			]
-		};
-		$parentElement.find('.khanoumi-carousel__content').slick($slickArgs);
-	}
+				]
+			};
+			$parentElement.find('.khanoumi-carousel__content').slick($slickArgs);
+		}
+	});
 })(jQuery);
