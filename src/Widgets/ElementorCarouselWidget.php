3aafda1b124856c531b517efa08b0d1e23b8b7ad
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-04 02:17:32 +0330

    #2 Add settings control for categories in Elementor widget

--- src/Widgets/ElementorCarouselWidget.php
+++ src/Widgets/ElementorCarouselWidget.php
@@ -2,6 +2,7 @@
 
 namespace KhanoumiAffiliatePartner\Widgets;
 
+use KhanoumiAffiliatePartner\Helpers\FiltersHelper;
 use KhanoumiAffiliatePartner\Helpers\ProductsHelper;
 
 class ElementorCarouselWidget extends \Elementor\Widget_Base
@@ -57,12 +58,48 @@ class ElementorCarouselWidget extends \Elementor\Widget_Base
 	}
 
 	/**
+	 * Adds input fields for customizing widget settings.
+	 *
+	 * @return	void
+	 */
+	protected function register_controls()
+	{
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => esc_html__('Products', 'khanoumi-affiliate-partner'),
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$categories = ['0' => esc_html__('All', 'khanoumi-affiliate-partner')];
+		foreach (FiltersHelper::getAllCategories() as $category)
+			$categories[$category['id']] = $category['name'];
+
+		$this->add_control(
+			'category',
+			[
+				'type'    => \Elementor\Controls_Manager::SELECT,
+				'label'   => esc_html__('Category', 'khanoumi-affiliate-partner'),
+				'options' => $categories,
+				'default' => '0',
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
 	 * Renders widget output on the frontend.
 	 *
 	 * @return	void
 	 */
 	protected function render()
 	{
-		echo ProductsHelper::getProducts();
+		$settings = $this->get_settings_for_display();
+
+		echo ProductsHelper::getProducts([
+			'category' => !empty($settings['category']) ? intval($settings['category']) : 0,
+		]);
 	}
 }
