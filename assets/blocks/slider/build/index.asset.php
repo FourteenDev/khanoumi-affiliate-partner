4b63ea9f6858f6fd75edd4d40b5713edc26fc580
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2026-08-13 09:28:15 +0330

    Build slider with new packages

--- assets/blocks/slider/build/index.asset.php
+++ assets/blocks/slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bb54d90e9d7eb98bd6c0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ed25c5066195c8e5818e');
--- assets/blocks/slider/build/view.asset.php
+++ assets/blocks/slider/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '3ff53c4cee26e414ce38');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
